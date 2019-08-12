19d6499860863afc51903b37d89c6f14eedcea0e
clarkmyfancy <clark.jack0@gmail.com>
2019-08-11 19:33:39 -0500

    impl insert, update, getNight, deleteAllRows, getAllNights & getTonight

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,33 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key") // :thing is how you reference a parameter from the function
+    fun getNight(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun deleteAllRows()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    // because of room, we only need to make this call one time and because it is live data
+    // we only need to attach an observer to it and it will change whenever the data in the db changes
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
+
