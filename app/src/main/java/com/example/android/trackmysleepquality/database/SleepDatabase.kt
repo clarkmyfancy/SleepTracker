11fe257de586de200e168d894a918b206d23b9f6
clarkmyfancy <clark.jack0@gmail.com>
2019-08-12 14:17:17 -0500

    created database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,56 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import java.security.AccessControlContext
+
+// for each new table, just add another class to the list of entities
+// for the version number, if you change the schema, you must up the version number or it wont work
+    // anymore
+// for the last, exportSchema is true by default
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    // associate the database with the dao
+    // this app only uses one table and one dao, you can add many of both
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    // allows clients to access the methods for creating and getting the database without instantiating
+        // the class
+    // since the only purpose of this is to privide us with a database, there is no neec to ever instantiate it
+    companion object {
+
+        // volitile make sure that the value of INSTANCE is always up to date and the same to all execution threads
+            // value of INSTANCE will never be cached and all writes and reads will always be done to mainmemory
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null // keeps a ref to the db, help to keep us from repetedly opening connections to the db bc its expensive
+
+        fun getInstance(context: Context) : SleepDatabase {
+            // ensures only one db gets initialized
+            // pass this into syncronized so that we have access to the context
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext, // context passed into the synch block
+                            SleepDatabase::class.java, // which db to build
+                            "sleep_history_database" // the name of the db
+                    )
+                            .fallbackToDestructiveMigration()
+                            // to avoid creating a migration object and strategy (migration is a way to convert old data in old table to new data that fits into new table, possilby because we changed the new tables by removbing columns)
+                            .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+
+
+    }
+}
